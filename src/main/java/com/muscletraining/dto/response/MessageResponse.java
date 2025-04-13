f86ad6aaf1e204203e2e533d6045f08cb36a52f0
fujiyuto <yuto.fujiwara.itfarm@gmail.com>
2025-04-13 20:21:22 +0900

    メッセージレスポンス作成

---
+++ src/main/java/com/muscletraining/dto/response/MessageResponse.java
@@ -0,0 +1,11 @@
+package com.muscletraining.dto.response;
+
+import lombok.Data;
+
+@Data
+public class MessageResponse {
+    /**
+     * メッセージ
+     */
+    private String message;
+}
--- src/main/java/com/muscletraining/validation/PasswordMatches/PasswordMatches.java
+++ src/main/java/com/muscletraining/validation/PasswordMatches/PasswordMatches.java
@@ -1,6 +1,8 @@
 package com.muscletraining.validation.PasswordMatches;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import jakarta.validation.Constraint;
@@ -9,6 +11,7 @@ import jakarta.validation.Payload;
 
 @Constraint(validatedBy = PasswordMatchesValidator.class)
 @Target( {ElementType.TYPE} )
+@Retention(RetentionPolicy.RUNTIME)
 public @interface PasswordMatches {
     String message() default "{validation.password.mismatch}";
     Class<?>[] groups() default {};
